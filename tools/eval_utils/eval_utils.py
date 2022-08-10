b3312f3791314f7bdf5b591275a76e45831cdc29
Jin Heo <993jin@gmail.com>
2022-08-09 23:34:33 -0400

    feat: update eval_util for F1 score

--- tools/eval_utils/eval_utils.py
+++ tools/eval_utils/eval_utils.py
@@ -104,8 +104,8 @@ def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, sa
     logger.info('Average predicted number of objects(%d samples): %.3f'
                 % (len(det_annos), total_pred_objects / max(1, len(det_annos))))
 
-    with open(result_dir / 'result.pkl', 'wb') as f:
-        pickle.dump(det_annos, f)
+    #with open(result_dir / 'result.pkl', 'wb') as f:
+    #    pickle.dump(det_annos, f)
 
     result_str, result_dict = dataset.evaluation(
         det_annos, class_names,
@@ -114,11 +114,118 @@ def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, sa
     )
 
     logger.info(result_str)
-    ret_dict.update(result_dict)
+    #ret_dict.update(result_dict)
 
     logger.info('Result is save to %s' % result_dir)
     logger.info('****************Evaluation done.*****************')
     return ret_dict
+
+
+def eval_precision(cfg, model, dataloader, epoch_id, logger, dist_test=False, save_to_file=False, result_dir=None):
+    final_output_dir = result_dir / 'final_result' / 'data'
+
+    ## Init...
+    result_dir.mkdir(parents=True, exist_ok=True)
+    metric = {'gt_num': 0,}
+    for cur_thresh in cfg.MODEL.POST_PROCESSING.RECALL_THRESH_LIST:
+        metric['recall_roi_%s' % str(cur_thresh)] = 0
+        metric['recall_rcnn_%s' % str(cur_thresh)] = 0
+    dataset = dataloader.dataset
+    class_names = dataset.class_names
+    det_annos = []
+
+    total_prediction = 0
+    total_roi03      = 0
+    total_roi05      = 0
+    total_roi07      = 0
+    total_gt         = 0
+
+    ## Start evaluation
+    logger.info('*************** EPOCH %s EVALUATION *****************' % epoch_id)
+    model.eval()
+
+    progress_bar = tqdm.tqdm(total=len(dataloader), leave=True, desc='eval', dynamic_ncols=True)
+    start_time = time.time()
+    for i, batch_dict in enumerate(dataloader):
+        load_data_to_gpu(batch_dict)
+        with torch.no_grad():
+            pred_dicts, ret_dict = model(batch_dict)
+            for pred_idx in range(0, len(pred_dicts)):
+                total_prediction += len(pred_dicts[pred_idx]['pred_labels'])
+            total_roi03 += ret_dict['roi_0.3']
+            total_roi05 += ret_dict['roi_0.5']
+            total_roi07 += ret_dict['roi_0.7']
+            total_gt    += ret_dict['gt']
+        disp_dict = {}
+
+        statistics_info(cfg, ret_dict, metric, disp_dict)
+        annos = dataset.generate_prediction_dicts(
+            batch_dict, pred_dicts, class_names,
+            output_path=final_output_dir if save_to_file else None
+        )
+        det_annos += annos
+        if cfg.LOCAL_RANK == 0:
+            progress_bar.set_postfix(disp_dict)
+            progress_bar.update()
+    progress_bar.close()
+
+    print("total prediction ", total_prediction)
+    print("total roi03 ", total_roi03)
+    print("total roi05 ", total_roi05)
+    print("total roi07 ", total_roi07)
+    print("total gt ", total_gt)
+
+    logger.info('*************** Performance of EPOCH %s *****************' % epoch_id)
+    sec_per_example = (time.time() - start_time) / len(dataloader.dataset)
+    logger.info('Generate label finished(sec_per_example: %.4f second).' % sec_per_example)
+    logger.info(f'Total predictions {total_prediction}, GT {total_gt}, roi03 {total_roi03}, roi05 {total_roi05}, roi07 {total_roi07}')
+    precision03 = total_roi03 / total_prediction
+    precision05 = total_roi05 / total_prediction
+    precision07 = total_roi07 / total_prediction
+    recall03 = total_roi03 / total_gt
+    recall05 = total_roi05 / total_gt
+    recall07 = total_roi07 / total_gt
+    f103 = 2*(recall03*precision03) / (recall03 + precision03)
+    f105 = 2*(recall05*precision05) / (recall05 + precision05)
+    f107 = 2*(recall07*precision07) / (recall07 + precision07)
+
+    logger.info(f'Precision 03 ({precision03}), 05 ({precision05}), 07 ({precision07})')
+    logger.info(f'Recall 03 ({recall03}), 05 ({recall05}), 07 ({recall07})')
+    logger.info(f'F1 03 ({f103}), 05 ({f105}), 07 ({f107})')
+
+    ret_dict = {}
+
+    gt_num_cnt = metric['gt_num']
+    for cur_thresh in cfg.MODEL.POST_PROCESSING.RECALL_THRESH_LIST:
+        cur_roi_recall = metric['recall_roi_%s' % str(cur_thresh)] / max(gt_num_cnt, 1)
+        cur_rcnn_recall = metric['recall_rcnn_%s' % str(cur_thresh)] / max(gt_num_cnt, 1)
+        ret_dict['recall/roi_%s' % str(cur_thresh)] = cur_roi_recall
+        ret_dict['recall/rcnn_%s' % str(cur_thresh)] = cur_rcnn_recall
+
+    total_pred_objects = 0
+    for anno in det_annos:
+        total_pred_objects += anno['name'].__len__()
+    print("===== total predictions: ", total_pred_objects)
+    print("===== total groundtruth objects: ", gt_num_cnt)
+    logger.info('Average predicted number of objects(%d samples): %.3f'
+                % (len(det_annos), total_pred_objects / max(1, len(det_annos))))
+
+    #with open(result_dir / 'result.pkl', 'wb') as f:
+    #    pickle.dump(det_annos, f)
+
+    result_str, result_dict = dataset.evaluation(
+        det_annos, class_names,
+        eval_metric=cfg.MODEL.POST_PROCESSING.EVAL_METRIC,
+        output_path=final_output_dir
+    )
+
+    logger.info(result_str)
+    #ret_dict.update(result_dict)
+
+    logger.info('Result is save to %s' % result_dir)
+    logger.info('****************Evaluation done.*****************')
+    return ret_dict
+
 
 
 if __name__ == '__main__':
